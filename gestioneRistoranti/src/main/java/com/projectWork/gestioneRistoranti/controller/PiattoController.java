28f8b0af0ad5f9d69011707e823ba64e95a89970
Luca Di Giulio <lkdgl95@gmail.com>
2025-02-28 18:04:24 +0100

    modifica controller e entità piatto

--- gestioneRistoranti/src/main/java/com/projectWork/gestioneRistoranti/controller/PiattoController.java
+++ gestioneRistoranti/src/main/java/com/projectWork/gestioneRistoranti/controller/PiattoController.java
@@ -1,5 +1,6 @@
 package com.projectWork.gestioneRistoranti.controller;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.projectWork.gestioneRistoranti.auth.TokenService;
 import com.projectWork.gestioneRistoranti.model.Categoria;
@@ -54,7 +56,7 @@ public class PiattoController {
 	 * @return -> messaggio, in caso di successo
 	 */
 	@PostMapping("/{id}/nuovo")
-	public Object createPiatto(@RequestBody Piatto nuovoPiatto, @PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
+	public Object createPiatto(Piatto nuovoPiatto, @PathVariable Long id, HttpServletRequest request, HttpServletResponse response, @RequestParam("file") MultipartFile file) throws IOException {
 		// authentication
 		Utente authUtente = getAuthenticatedUtente(request);
 		if (authUtente == null) {
@@ -73,6 +75,8 @@ public class PiattoController {
 			return Collections.singletonMap("messsage", "Categoria non trovata");
 		}
 		
+		nuovoPiatto.setNomeFoto(file.getOriginalFilename());
+		nuovoPiatto.setFoto(file.getBytes());
 		nuovoPiatto.setCategoria(findCat.get());
 		piattoRepository.save(nuovoPiatto);
 		return Collections.singletonMap("message", "Piatto aggiunto con successo!");
@@ -110,8 +114,8 @@ public class PiattoController {
 	 * errore
 	 */
 	@PutMapping("/{id}")
-	public Object updatePiatto(@PathVariable Long id, @RequestBody Piatto piattoDetails, HttpServletResponse response,
-			HttpServletRequest request) {
+	public Object updatePiatto(@PathVariable Long id, Piatto piattoDetails, HttpServletResponse response,
+			HttpServletRequest request, @RequestParam("file") MultipartFile file) throws IOException {
 		Utente authUtente = getAuthenticatedUtente(request);
 		if (authUtente == null) {
 			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -130,6 +134,8 @@ public class PiattoController {
 		}
 		
 		Piatto p = piatto.get();
+		p.setFoto(file.getBytes());
+		p.setNomeFoto(file.getOriginalFilename());
 		p.setNome(piattoDetails.getNome());
 		p.setCosto(piattoDetails.getCosto());
 		p.setDescrizione(piattoDetails.getDescrizione());
